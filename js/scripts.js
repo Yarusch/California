ae3a6382fb774086addf6fccad8a89521ca5d15b
Yarusch <impulsimp13@gmail.com>
2023-03-07 02:45:35 +0100

    California

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:60,
+        nav:true,
+        navText: [ '', ' ' ],
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
