9d0adc997bb2c59d0091752acfef24cd5ea4f28a
Yarusch <impulsimp13@gmail.com>
2023-03-10 05:13:16 +0100

    add styles

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,11 @@ $(document).ready(function(){
             }
         }
     });
+    $('.burger-menu').click(function (event) {
+        $('.burger-menu,nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.drop_btn').click(function (event) {
+        $('.drop_list').toggleClass('active');
+    })
 });
